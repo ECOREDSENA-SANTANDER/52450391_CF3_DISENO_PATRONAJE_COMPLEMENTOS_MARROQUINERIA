217642611ff4631b2cbec449f543641ee7e54b76
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2025-05-08 19:08:57 -0500

    Act

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,8 +11,8 @@
 
     p.mb-4(data-aos="fade-left") La marroquinería es un arte y una industria que combina diseño, funcionalidad y precisión en la confección de productos de cuero y otros materiales. Para garantizar la calidad y resistencia de estos artículos, es fundamental seguir un proceso técnico estructurado que abarca desde el diseño hasta el ensamblaje final; por tal razón, se presenta una guía paso a paso que facilitará la confección de productos de marroquinería, abordando técnicas clave como el armado, la costura y la aplicación de acabados.
     
-    .row.fondoi.align-items-center.p-5.mb-0(data-aos="fade-down")
-      .col-lg-12.col-md-12.col-12
+    .row.fondoi.align-items-center.p-0.mb-0(data-aos="fade-down")
+      .col-lg-12.col-md-12.col-12.px-5
 
         .row.justify-content-center.align-items-center.mb-0          
           .col-lg-12
